7bcebd1722aeacd936c9a12d9f25a4f654f960f2
Fabien V <fabien.vermeiren@gmail.com>
2022-04-12 12:30:42 +0200

    add useCountdownTimer from npm

--- src/js/App.js
+++ src/js/App.js
@@ -1,18 +1,29 @@
 import Timer from "./Timer";
+import React from "react";
+import {useCountdownTimer} from "use-countdown-timer";
+import {formatTime} from "./formatTime";
 
 export function App() {
-    const defaultTime = 60;
+    const defaultTimer = 65;
+    const {countdown, start, reset, pause, isRunning} = useCountdownTimer({
+        timer: 1000 * defaultTimer,
+    });
+    let formatCountDown = formatTime(countdown / 1000);
     return (
         <div className="App">
             <header>
                 <h1>Pomodoro</h1>
             </header>
             <main>
-                <Timer timeInSec={defaultTime} />
-                <button>+</button>
-                <button>-</button>
-                <button>start</button>
-                <button>reset</button>
+                <React.Fragment>
+                    <div>{formatCountDown}</div>
+                    <button onClick={reset}>Reset</button>
+                    {isRunning ? (
+                        <button onClick={pause}>Pause</button>
+                    ) : (
+                        <button onClick={start}>Start</button>
+                    )}
+                </React.Fragment>
             </main>
         </div>
     );
