ee7e1a05fe3864e88145c66fea1e01772d3ad7e3
Fabien V <fabien.vermeiren@gmail.com>
2022-04-12 11:36:05 +0200

    basic html

--- src/js/App.js
+++ src/js/App.js
@@ -1,3 +1,16 @@
 export function App() {
-  return <h1>Hello world!</h1>;
+    return (
+        <div className="App">
+            <header>
+                <h1>Pomodoro</h1>
+            </header>
+            <main>
+                <h2>20:00</h2>
+                <button>+</button>
+                <button>-</button>
+                <button>start</button>
+                <button>reset</button>
+            </main>
+        </div>
+    );
 }
