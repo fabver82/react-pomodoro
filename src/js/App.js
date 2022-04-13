82f7b291ffdeb4de23dfe62679c2c779d6430170
Fabien V <fabien.vermeiren@gmail.com>
2022-04-13 09:18:40 +0200

    remove countdown packages and make my component

--- src/js/App.js
+++ src/js/App.js
@@ -8,7 +8,7 @@ export function App() {
                 <h1>Pomodoro</h1>
             </header>
             <main>
-                <Timer initialTime={60} />
+                <Timer />
             </main>
         </div>
     );
--- src/js/Timer.js
+++ src/js/Timer.js
@@ -1,21 +1,25 @@
 import React, {useState, useEffect} from "react";
 import {formatTime} from "./formatTime";
-import {useCountdownTimer} from "use-countdown-timer";
 
-export default function Timer({initialTime}) {
-    const {countdown, start, reset, pause, isRunning} = useCountdownTimer({
-        timer: 1000 * initialTime,
-    });
+export default function Timer() {
+    // const [minutes, setMinutes] = useState(1);
+    const [seconds, setSeconds] = useState(5);
+    const [hasEnded, setHasEnded] = useState(false);
 
+    useEffect(() => {
+        let interval = setInterval(() => {
+            clearInterval(interval);
+
+            if (seconds === 0) {
+                //show modal
+            } else {
+                setSeconds(seconds - 1);
+            }
+        }, 1000);
+    }, [seconds]);
     return (
-        <React.Fragment>
-            <div>{formatTime(countdown / 1000)}</div>
-            <button onClick={reset}>Reset</button>
-            {isRunning ? (
-                <button onClick={pause}>Pause</button>
-            ) : (
-                <button onClick={start}>Start</button>
-            )}
-        </React.Fragment>
+        <div>
+            <div>{formatTime(seconds)}</div>
+        </div>
     );
 }
