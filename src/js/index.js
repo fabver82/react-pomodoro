fa6c461131fab204ca642e5b3274b0b5ebd02be7
Fabien V <fabien.vermeiren@gmail.com>
2022-04-14 09:37:05 +0200

    add sass and fix issue with autoinstall from parcel

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {App} from "./App";
-
+import "../sass/style.scss";
 // const app = document.getElementById("app");
 // ReactDOM.render(<App />, app);
 const root = ReactDOM.createRoot(document.getElementById("app"));
